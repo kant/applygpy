ff0dcc2c402f286a010a19ca45b0dafe7fd64a0d
mzwiessele <ibinbei@gmail.com>
2015-09-30 13:46:54 +0100

    [matplotlib] maybe it works now on travis?

--- applygpy/__init__.py
+++ applygpy/__init__.py
@@ -3,7 +3,11 @@ Created on 29 Sep 2015
 
 @author: Max Zwiessele
 '''
-
+try:
+    import matplotlib  # @UnresolvedImport
+    matplotlib.use('Agg')
+except:
+    pass
 import data
 import model_selection
 import prediction
