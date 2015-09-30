652f0670b937451090906781dcb467616333d326
mzwiessele <ibinbei@gmail.com>
2015-09-30 14:06:11 +0100

    [matplotlib] added screen to travis

--- applygpy/__init__.py
+++ applygpy/__init__.py
@@ -3,11 +3,7 @@ Created on 29 Sep 2015
 
 @author: Max Zwiessele
 '''
-try:
-    import matplotlib  # @UnresolvedImport
-    matplotlib.use('Agg')
-except:
-    pass
-import data
-import model_selection
-import prediction
+
+from . import data
+from . import model_selection
+from . import prediction
--- applygpy/tests/__init__.py
+++
@@ -1,2 +0,0 @@
-import matplotlib  # @UnresolvedImport
-matplotlib.use('Agg')
