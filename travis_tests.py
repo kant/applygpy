bf1a58bcba85351249bebe77853c5f4f124ae084
Max Zwiessele <ibinbei@gmail.com>
2015-11-09 08:32:34 +0000

    Update travis_tests.py

--- travis_tests.py
+++ travis_tests.py
@@ -28,6 +28,8 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #===============================================================================
+import matplotlib
+matplotlib.use('agg')
 
 import nose
 nose.main('applygpy', defaultTest='applygpy/tests')
