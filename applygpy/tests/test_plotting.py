8017604d1599a0858c329027375cf7cf601cf557
mzwiessele <ibinbei@gmail.com>
2015-11-09 08:54:26 +0000

    [plotting] importerror

--- applygpy/tests/test_plotting.py
+++ applygpy/tests/test_plotting.py
@@ -3,7 +3,7 @@ Created on 30 Sep 2015
 
 @author: Max Zwiessele
 '''
-import matplotlib, 
+import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt  # @UnresolvedImport
 
@@ -22,7 +22,7 @@ class Test(unittest.TestCase):
         plt.close('all')
 
     def testPlotting(self):
-        m = GPy.models.GPRegression(self.X, self.Y) 
+        m = GPy.models.GPRegression(self.X, self.Y)
         p = PredictionModel(m)
         fig, ax = plt.subplots()
         m.plot(plot_training_data=False, ax=ax)
@@ -33,17 +33,17 @@ class Test(unittest.TestCase):
         i1.seek(0)
 
         fig, ax = plt.subplots()
-        p.plot(plot_training_data=False, ax=ax)        
+        p.plot(plot_training_data=False, ax=ax)
         ax.set_ylim(0, 1)
         ax.set_xlim(-2, 2)
         i2 = StringIO()
         fig.savefig(i2, format='svg')
         i2.seek(0)
-        
+
         self.assertEqual(i1.read(), i2.read())
 
     def testPlottingSparse(self):
-        m = GPy.models.SparseGPRegression(self.X, self.Y) 
+        m = GPy.models.SparseGPRegression(self.X, self.Y)
         p = PredictionModelSparse(m)
         fig, ax = plt.subplots()
         m.plot(plot_training_data=False, ax=ax)
@@ -54,17 +54,17 @@ class Test(unittest.TestCase):
         i1.seek(0)
 
         fig, ax = plt.subplots()
-        p.plot(plot_training_data=False, ax=ax)        
+        p.plot(plot_training_data=False, ax=ax)
         ax.set_ylim(0, 1)
         ax.set_xlim(-2, 2)
         i2 = StringIO()
         fig.savefig(i2, format='svg')
         i2.seek(0)
-        
+
         self.assertEqual(i1.read(), i2.read())
 
     def testPlottingClass(self):
-        m = GPy.models.GPClassification(self.X, self.Y<0) 
+        m = GPy.models.GPClassification(self.X, self.Y<0)
         p = PredictionModel(m)
         fig, ax = plt.subplots()
         m.plot(plot_training_data=False, ax=ax)
@@ -75,17 +75,17 @@ class Test(unittest.TestCase):
         i1.seek(0)
 
         fig, ax = plt.subplots()
-        p.plot(plot_training_data=False, ax=ax)        
+        p.plot(plot_training_data=False, ax=ax)
         ax.set_ylim(0, 1)
         ax.set_xlim(-2, 2)
         i2 = StringIO()
         fig.savefig(i2, format='svg')
         i2.seek(0)
-        
+
         self.assertEqual(i1.read(), i2.read())
-        
+
     def testPlottingSparseClass(self):
-        m = GPy.models.SparseGPClassification(self.X, self.Y<0) 
+        m = GPy.models.SparseGPClassification(self.X, self.Y<0)
         p = PredictionModelSparse(m)
         fig, ax = plt.subplots()
         m.plot(plot_training_data=False, ax=ax)
@@ -96,21 +96,21 @@ class Test(unittest.TestCase):
         i1.seek(0)
 
         fig, ax = plt.subplots()
-        p.plot(plot_training_data=False, ax=ax)        
+        p.plot(plot_training_data=False, ax=ax)
         ax.set_ylim(0, 1)
         ax.set_xlim(-2, 2)
         i2 = StringIO()
         fig.savefig(i2, format='svg')
         i2.seek(0)
-        
+
         self.assertEqual(i1.read(), i2.read())
 
     def testPlottingDataNotShow(self):
-        m = GPy.models.SparseGPRegression(self.X, self.Y) 
+        m = GPy.models.SparseGPRegression(self.X, self.Y)
         p = PredictionModelSparse(m)
         p.plot_data()
-        
-        fig, ax = plt.subplots()        
+
+        fig, ax = plt.subplots()
         p.plot(plot_training_data=False, ax=ax)
         ax.set_ylim(0, 1)
         ax.set_xlim(-2, 2)
@@ -119,16 +119,16 @@ class Test(unittest.TestCase):
         i1.seek(0)
 
         fig, ax = plt.subplots()
-        p.plot(plot_training_data=True, ax=ax)        
+        p.plot(plot_training_data=True, ax=ax)
         ax.set_ylim(0, 1)
         ax.set_xlim(-2, 2)
         i2 = StringIO()
         fig.savefig(i2, format='svg')
         i2.seek(0)
-        
+
         self.assertEqual(i1.read(), i2.read())
 
-        m = GPy.models.GPRegression(self.X, self.Y) 
+        m = GPy.models.GPRegression(self.X, self.Y)
         p = PredictionModel(m)
         p.plot_data()
 
@@ -141,15 +141,15 @@ class Test(unittest.TestCase):
         i1.seek(0)
 
         fig, ax = plt.subplots()
-        p.plot(plot_training_data=True, ax=ax)        
+        p.plot(plot_training_data=True, ax=ax)
         ax.set_ylim(0, 1)
         ax.set_xlim(-2, 2)
         i2 = StringIO()
         fig.savefig(i2, format='svg')
         i2.seek(0)
-        
+
         self.assertEqual(i1.read(), i2.read())
-        
+
 
 
 if __name__ == "__main__":
